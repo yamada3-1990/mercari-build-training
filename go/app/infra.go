6000b78c344dc9cf8fc1b86f0138a0e6b8df81de
yamada3-1990 <k.ae.de19900220+1@gmail.com>
2025-03-11 13:38:17 +0900

    reflect the changes made in step 5

--- go/app/infra.go
+++ go/app/infra.go
@@ -38,66 +38,40 @@ type itemRepository struct {
 // nilのitemRepoを使用したことによるnil参照panicを防ぐ
 func NewItemRepository(db *sql.DB) (ItemRepository, error) {
 	// items tableがなかったら作成
-	query := `
-                                CREATE TABLE IF NOT EXISTS items (
-                                                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                                name TEXT NOT NULL,
-                                                category_id INTEGER,
-                                                image_name TEXT NOT NULL,
-                                                FOREIGN KEY (category_id) REFERENCES categories(id)
-                                );
-                        `
-	_, err := db.Exec(query)
+	q, err := os.ReadFile("db/items.sql")
 	if err != nil {
-		slog.Error("failed to create items table", "error", err)
+		return &itemRepository{}, err
+	}
+
+	query := string(q)
+	_, err = db.Exec(query)
+	if err != nil {
+		slog.Error("failed to create items table and categories table", "error", err)
 		return nil, err
 	}
 
-	// categories tableが無かったら作成
-	query = `
-                                CREATE TABLE IF NOT EXISTS categories (
-                                                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                                name TEXT NOT NULL UNIQUE
-                                );
-                        `
-	_, err = db.Exec(query)
-	if err != nil {
-		slog.Error("failed to create categories table: ", "error", err)
-		return nil, err
-	}
 	return &itemRepository{db: db}, nil
 }
 
 func (i *itemRepository) Insert(ctx context.Context, item *Item) error {
-	var categoryID int
-
-	// categories tableから(categories tableの)name = item.Categoryのidを取得
-	err := i.db.QueryRow("SELECT id FROM categories WHERE name = ?", item.Category).Scan(&categoryID)
-	if err != nil {
-		if err == sql.ErrNoRows {
-			// 該当する行がなかったら = 新しいカテゴリーだったら
-			// categories tableのnameにitem.Categoryの値をinsert
-			res, err := i.db.Exec("INSERT INTO categories (name) VALUES (?)", item.Category)
-			if err != nil {
-				return err
-			}
-			// 最後に挿入された自動採番(AUTOINCREMENT)のidを取得
-			id, err := res.LastInsertId()
-			if err != nil {
-				return err
-			}
-			categoryID = int(id)
-		} else {
-			return err
-		}
-	}
-	// insert
-	query := "INSERT INTO items (name, category_id, image_name) VALUES (?, ?, ?)"
-	_, err = i.db.Exec(query, item.Name, categoryID, item.Image)
+	// 該当する行がなかったら = 新しいカテゴリーだったらcategoryを挿入
+	query := `INSERT OR IGNORE INTO categories (name) VALUES (?)`
+	_, err := i.db.Exec(query, item.Category)
 	if err != nil {
 		return err
 	}
 
+	// まとめてinsert
+	query = `
+			INSERT INTO items (name, category_id, image_name)
+				SELECT ?, categories.id, ?
+				FROM categories
+				WHERE categories.name = ?
+			`
+	_, err = i.db.Exec(query, item.Name, item.Image, item.Category)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -202,55 +176,3 @@ func (i *itemRepository) SearchItemsByKeyword(ctx context.Context, keyword strin
 
 	return items, nil
 }
-
-/*
-
-*** STEP 4 ***
-GETとPOSTのリクエストの違いについて調べてみましょう
-	->GET:  サーバーにリクエストを送信、リソースを取得
-	->POST: サーバーにデータを送信、リソースの更新など
-
-ブラウザで http://127.0.0.1:9000/items にアクセスしても {"message": "item received: <name>"} が返ってこないのはなぜでしょうか？
-	-> server.go の route に GET /items がないから？
-
-アクセスしたときに返ってくるHTTPステータスコードはいくつですか？
-	-> 200 OK
-
-それはどんな意味をもつステータスコードですか？
-	-> リクエストが正常に処理された
-
-ハッシュ化とはなにか？
-	-> 特定のルール(ハッシュ関数)に基づいて値を変換すること
-
-SHA-256 以外にどんなハッシュ関数があるか調べてみましょう
-	-> SHA-3, MD5など >アルゴリズムの設計、セキュリティ強度、速度、用途が違う らしい
-
-Log levelとは？
-	-> ソフトウェアが記録するログ(どんな動作が行われたかの記録)の詳細度と重要度を調整するための仕組み
-
-webサーバーでは、本番はどのログレベルまで表示する？
-	-> INFO以上が一般的 開発環境だとDEBUG
-
-port (ポート番号)
-	-> コンピュータが通信に使用するプログラムを識別するための番号 HTTP:80 etc.
-
-localhost, 127.0.0.1
-	-> localhost: コンピューター自身を指し示すためのホスト名
-	-> 127.0.0.1: IPv4における特別なIPアドレス
-
-HTTPリクエストメソッド
-	-> Webサーバーにどのような処理をするかを伝える役割
-	-> GET/POST/PUT(更新)/PATCH(一部更新)/DELETE(削除)
-
-HTTPステータスコード (1XX, 2XX, 3XX, 4XX, 5XXはそれぞれどんな意味を持ちますか？)
-	-> 1XX: リクエストが受け付けられて処理が続いている(Informational)
-	-> 2XX: リクエストが正常に完了(Success)
-	-> 3XX: リクエストを完了するために追加のアクションが必要(Redirection)
-	-> 4XX: リクエストに問題あり(Client Error)
-	-> 5XX: サーバーがリクエストを処理できなかった(Server Error)
-
-*/
-
-// curlじゃなくて curl.exe で実行
-// cd go してから go run cmd/api/main.go でサーバーを起動するなら
-// main.go の実行ディレクトリは go/
